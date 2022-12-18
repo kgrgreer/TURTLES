a22258ec5e9d1ab5719f85765a9ecf55a33720d9
Kevin Greer <kgrgreer@gmail.com>
2022-12-18 11:07:06 -0500

    Start of whitespace and comment support to jsparser.

--- src/jsparser.js
+++ src/jsparser.js
@@ -53,6 +53,9 @@ scope.eval$(`
     [ '_ 'a 'z' range 'A 'Z range '0 '9 range ] alt
     0 repeat &join mapp
   ] seq &join mapp }                                              :lhs
+  { o | [ "  " `  nl ] alt 0 repeat }                             :space
+  { o | [ '// nl notChars 0 repeat nl ] seq }                     :comment
+  { o | [ o .space  o .comment ] 0 repeat }                       :ignore
   | { | ?? }
 } :FormulaParser
 
